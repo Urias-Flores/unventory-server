368279d9e442b77de3a954f3484fc35d7dcfe3d3
Urias-Flores <floresmeza808@gmail.com>
2026-03-03 21:20:20 -0600

    fix: lint

--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -11,7 +11,7 @@ export class SupplierService {
   ) {}
 
   async findAllSuppliers(
-    populate: [],
+    populate: string[],
     filters: object,
   ): Promise<SupplierEntity[]> {
     try {
@@ -28,7 +28,7 @@ export class SupplierService {
 
   async findAllSupplierById(
     id: number,
-    populate: [],
+    populate: string[],
     filters: object,
   ): Promise<SupplierEntity> {
     try {
