ccf34e863c79d36998484b3a772633c29908b09f
Urias-Flores <floresmeza808@gmail.com>
2026-03-03 21:29:55 -0600

    fix: module not found

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'unventory-api',
       script: 'dist/main.js',
       instances: 1,
+      node_args: '--experimental-global-webcrypto',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
